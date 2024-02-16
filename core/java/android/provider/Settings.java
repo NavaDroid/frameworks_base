LeafOS-Project/android_frameworks_base

SystemUI: Make round QS toggleable [1/3]
Change-Id: I86e3bc990b78e763bfc9fb4a7ed8ffeeff95cc3f

core/java/android/provider/Settings.java
@@ -11172,6 +11172,12 @@ public final class Settings {
         public static final String QS_TILE_SHAPE = "qs_tile_shape";
 
         /**
+         * Quicksettings UI Style
+         * @hide
+         */
+        public static final String QS_UI_STYLE = "qs_ui_style";
+
+        /**
          * Brightness slider styles
          * @hide
          */

NavaDroid/frameworks_base

core/java/android/provider/Settings.java
@@ -12525,6 +12525,12 @@ public final class Settings {
         public static final String QS_TILE_SHAPE = "qs_tile_shape";
 
         /**
+         * Quicksettings UI Style
+         * @hide
+         */
+        public static final String QS_STYLE_ROUND = "qs_style_round";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */